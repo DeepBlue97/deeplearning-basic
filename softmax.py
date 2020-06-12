44f4f13e9b702823004e2661fafe16b53f7abd8a
DeepBlue97 <48646415+DeepBlue97@users.noreply.github.com>
2020-06-13 00:12:53 +0800

    Create softmax.py

---
+++ softmax.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+
+a = np.array([0.3, 2.9, 4.0])
+
+exp_a = np.exp(a)  # 指数函数
+print(exp_a)
+
+sum_exp_a = np.sum(exp_a)  # 指数函数的和
+print(sum_exp_a)
+
+y = exp_a / sum_exp_a
+print(y)
+
+# 不能防止溢出版
+def softmax(a):
+    exp_a = np.exp(a)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+    return y
+
+# 防止溢出版
+def softmax(a):
+    c = np.max(a)
+    exp_a = np.exp(a-c)  # 溢出对策
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+    return y
+
+a = np.array([0.3, 2.9, 4.0])
+y = softmax(a)
+print(y)
+print(np.sum(y))
